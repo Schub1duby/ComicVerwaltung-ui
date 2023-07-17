31f118aa9c8fca9dc5fd0b0d04b23a1b7494fa03
cyrillschudel <cyrill.schudel@bluewin.ch>
2023-07-17 07:49:53 +0200

    setup completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ListComponent} from "./components/list/list.component";
+import {ExampleComponent} from "./components/example/example.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'liste', component: ListComponent, pathMatch: 'full'},
+  {path: 'example', component: ExampleComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ListComponent } from './components/list/list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExampleComponent } from './components/example/example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    ListComponent,
+    HeaderComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
